85ce7718a1f63ba0f94c1a0b9ec0137cb6a45133
Rakshana-raksh146 <rakshanadurairaj@gmail.com>
2020-02-25 22:53:06 +0530

    Forecast update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (long,lat,callback) => {
         else if(body.error){          //when long/lat is missing
             callback('Unable to find the location',undefined)
         }else{
-            callback(undefined ,body.daily.data[0].summary + "It is currently "+ body.currently.temperature +" degrees out." + "There is a " + body.currently.precipProbability + "% chance of rain. " )
+            callback(undefined ,body.daily.data[0].summary + "It is currently "+ body.currently.temperature +" degrees out. This high today " +  body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + "% chance of rain. " )
         }
     })
 }
